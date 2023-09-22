f744f8426908da55d852c0b8bbb22b8fe419b64c
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2023-09-22 19:58:19 -0300

    detalhes

--- segundaParte/Ex3/Aniversario.java
+++ segundaParte/Ex3/Aniversario.java
@@ -1,4 +1,5 @@
 public class Aniversario implements IMensagem {
+   @Override
    public String showMessagem(CartaoWEB p1) {
       String email = p1.get_email();
       String nome = p1.get_nome();
